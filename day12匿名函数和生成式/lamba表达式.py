defa3ecd7e977ddc2aab7ac999980e925486cebc
gongxiaoliao <gongxiaoliao@kailinjt.com>
2024-12-31 17:15:16 +0800

    修改lamba笔记

--- day12匿名函数和生成式/lamba表达式.py
+++ day12匿名函数和生成式/lamba表达式.py
@@ -1,6 +1,13 @@
 
+# 定义一个简单的Lambda函数，对传入的参数求平方
+square = lambda x: x * x
+
+# 调用Lambda函数
+result = square(5)
+print(result)  # 输出: 25
 
 
+# x,y 作为参数传入，返回x,y的和
 res = (lambda x,y:x+y)(1,3)
 
 print(res)
