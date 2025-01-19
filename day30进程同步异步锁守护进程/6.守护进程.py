a90f6437e04c11614db1f719ada6bb244db35f2f
gongxiaoliao <gongxiaoliao@kailinjt.com>
2025-01-19 20:26:21 +0800

    锁

--- day30进程同步异步锁守护进程/6.守护进程.py
+++ day30进程同步异步锁守护进程/6.守护进程.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     p = Process(target=test,args=('gong',))
     p.daemon = True  # 将该进程设置为守护进程，守护的程序挂了就直接挂了，p必须在start语句之前否则报错。
     p.start()
-    time.sleep(6) # 在cpu特别繁忙的情况下，这里的时间如果很短不会触发守护进程的内容
+    time.sleep(1) # 在cpu特别繁忙的情况下，这里的时间如果很短不会触发守护进程的内容
     print('挂了')  # 这里启动的就是守护的进程
